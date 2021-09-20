4c7cdbdc0dbadfb9f4d2e0e9c78afa9aa6eb4ae0
Nikita <nekytnp@gmail.com>
2021-09-20 13:11:59 +0300

    Исправлены ошибки в модели Теста

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,12 +7,13 @@ class Test < ApplicationRecord
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than: 0 }
 
+  scope :by_category, -> (category_name) { where(category: { title: category_name }) }
   scope :by_level, -> (level) { where(level: level) }
   scope :easy, -> { by_level(0..1) }
   scope :medium, -> { by_level(2..4) }
   scope :hard, -> { by_level(5..Float::INFINITY) }
 
-  scope :titles_by_category, -> (category_name) { 
-    joins(:category).where(category: { title: category_name }).pluck(:title).sort
-  }
+  def self.titles_by_category(category_name)
+    joins(:category).by_category(category_name).order(title: :desc).pluck(:title)
+  end
 end
