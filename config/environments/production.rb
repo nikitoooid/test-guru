2187c71c90679dbfddb350ab56a3ca702853e3b6
Nikita <nekytnp@gmail.com>
2021-12-14 18:20:57 +0200

    Изменены настройки ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'immense-castle-04207.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
+    address:              'mail.adm.tools',
+    port:                 465,
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
