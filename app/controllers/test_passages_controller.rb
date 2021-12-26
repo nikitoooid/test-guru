652d29a5b81b5f4c87ca9198d2b21012a481125e
Nikita <nekytnp@gmail.com>
2021-12-26 22:52:12 +0200

    Добавлена проверка таймера  при обновлении прохождения теста

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -29,7 +29,7 @@ class TestPassagesController < ApplicationController
   def update
     params[:answer_ids].nil? ? flash.now[:danger] = t('.no_answer') :  @test_passage.accept!(params[:answer_ids])
 
-    if @test_passage.completed?
+    if @test_passage.completed? || @time_left < 0
       TestsMailer.completed_test(@test_passage).deliver_now
       redirect_to result_test_passage_path(@test_passage)
     else
