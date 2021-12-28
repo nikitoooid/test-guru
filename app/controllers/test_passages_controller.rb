32d7cdd98d9568454d0e7fb14cc845b51d483314
Nikita <nekytnp@gmail.com>
2021-12-28 10:38:57 +0200

    Попытка исправить конфликты слияния

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -28,7 +28,7 @@ class TestPassagesController < ApplicationController
 
     params[:answer_ids].nil? ? flash.now[:danger] = t('.no_answer') :  @test_passage.accept!(params[:answer_ids])
 
-    if @test_passage.completed?
+    if @test_passage.completed? || @test_passage.expired?
       @received_badges = BadgeService.new(@test_passage).call
       TestsMailer.completed_test(@test_passage).deliver_now
       redirect_to result_test_passage_path(@test_passage)
